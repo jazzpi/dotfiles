e519d077abdaf21ee3db71d84faa7a5b744af48b
jazzpi <jasper@mezzo.de>
2016-04-11 12:00:06 +0200

    Install vim-plug

--- install.sh
+++ install.sh
@@ -17,10 +17,16 @@ function install_file {
 
 echo "Installing awesome config..."
 install_file awesome/rc.lua ~/.config/awesome/rc.lua
+echo "Note: Not installing the theme!"
 
 echo "Installing Neovim config..."
 install_file nvim/init.vim ~/.config/nvim/init.vim
 install_file nvim/ftplugin/markdown.vim ~/.config/nvim/ftplugin/markdown.vim
+echo "Downloading vim-plug"
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Installing plugins"
+nvim -c "PlugInstall | qa"
 
 echo "Installing Xresources..."
 install_file Xresources ~/.Xresources
