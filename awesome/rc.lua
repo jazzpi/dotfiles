5b38237b4f2ebf4d8c790b30c1d4aec1d4120966
jazzpi <jasper@mezzo.de>
2017-02-08 21:45:14 +0100

    awesome: Autostart

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -666,3 +666,29 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Autostart programs {{{
+
+function run_once(prg,arg_string,pname,screen)
+  if not prg then
+    do return nil end
+  end
+
+  if not pname then
+    pname = prg
+  end
+
+  if not arg_string then
+    awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. "' || (" .. prg .. ")",screen)
+  else
+    awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. " ".. arg_string .."' || (" .. prg .. " " .. arg_string .. ")",screen)
+  end
+end
+
+local AUTOSTART_PROGRAMS = {"thunderbird", "nm-applet", "ibus-daemon -r"}
+
+for index,cmd in pairs(AUTOSTART_PROGRAMS) do
+  run_once(cmd)
+end
+
+-- }}}
