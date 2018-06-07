361f1e2fda981bc46c041ba0b13aed5b104f81f1
jazzpi <jasper@mezzo.de>
2018-06-07 20:42:27 +0200

    awesome: Fix common.notify

--- awesome/mywidgets/common.lua
+++ awesome/mywidgets/common.lua
@@ -8,19 +8,27 @@ local notify = {
       err = naughty.config.presets.critical,
    },
 
-   dbg = function(text, title) return __call(text, title, _presets.dbg) end,
-   info = function(text, title) return __call(text, title, _presets.dbg) end,
-   warn = function(text, title) return __call(text, title, _presets.dbg) end,
-   err = function(text, title) return __call(text, title, _presets.dbg) end,
 }
-function notify.__call(self, text, title, preset)
+
+function notify.dbg(text, title)
+   return notify.__call(text, title, notify._presets.dbg)
+end
+function notify.info(text, title)
+   return notify.__call(text, title, notify._presets.info)
+end
+function notify.warn(text, title)
+   return notify.__call(text, title, notify._presets.warn)
+end
+function notify.err(text, title)
+   return notify.__call(text, title, notify._presets.err)
+end
+
+function notify.__call(text, title, preset)
    preset = preset or notify._presets.info
    naughty.notify({ preset = preset,
                     title = title,
                     text = tostring(text) })
 end
-
-setmetatable(notify, notify)
 
 return {
    notify = notify
