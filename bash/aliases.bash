efdd315b4ba1dcfd8df3862970a23429ef91f2c7
jazzpi <jasper@mezzo.de>
2019-08-09 14:34:39 +0200

    bash: Add some more aliases

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,5 +1,7 @@
 alias rsrc=". ~/.bashrc"
 
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
 alias la="ls -lAh"
 alias ll="ls -lh"
 alias lsa="ls -lah"
@@ -10,6 +12,7 @@ alias udm="udisksctl mount -b"
 alias udu="udisksctl unmount -b"
 
 alias g="git"
+alias gco="git checkout"
 alias gst="git status"
 alias gd="git diff"
 alias ga="git add"
