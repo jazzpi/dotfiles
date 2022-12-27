526955baccc12065966bb2015187750ed07350ad
jazzpi <jasper@mezzo.de>
2022-12-27 16:51:40 +0100

    sway: Turn off screen 60s after locking

--- sway/scripts/idle.sh
+++ sway/scripts/idle.sh
@@ -2,6 +2,6 @@
 
 swayidle \
     timeout 300 'swaylock --fade-in 5 --grace 5' \
-    timeout 600 'swaymsg "output * dpms off"' \
+    timeout 360 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' \
     before-sleep 'swaylock'
