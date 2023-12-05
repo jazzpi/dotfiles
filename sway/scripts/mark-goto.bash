0ec5c3736d0ad7ed24541454150b7c7b834a2cc8
jazzpi <jasper@mezzo.de>
2023-12-05 16:20:15 +0100

    sway: Fix mark/goto

--- sway/scripts/mark-goto.bash
+++ sway/scripts/mark-goto.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-marks=$(swaymsg -t get_marks | sed -e 's/\[//;s/"//g;s/,/ /g;s/\]//;1d;$d;s/^ \+//' | tr -d '\n')
+marks=$(swaymsg -t get_marks | sed -e 's/\[//;s/"//g;s/,/|/g;s/\]//;1d;$d;s/^ \+//' | tr -d '\n')
 echo "Jump to [$marks]: " | xxd
-echo "" | dmenu -p "Jump to [$marks]: " | xargs -i -r swaymsg '[con_mark=' {} ']' focus
+echo "$marks" | rofi -dmenu -sep '|' -p "Jump to [$marks]: " -only-match | xargs -i -r swaymsg '[con_mark=' {} ']' focus
