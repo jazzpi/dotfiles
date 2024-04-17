6eb71a20b41b6c2236c4c0a1cf60a175cb874b5e
jazzpi <jasper@mezzo.de>
2024-04-17 12:42:33 +0200

    hypr: Start whatever polkit agent is available

---
+++ hyprland/scripts/start-polkit-agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+declare -a possible_paths=(
+    "/usr/lib/polkit-kde-authentication-agent-1"
+    "/usr/libexec/kf5/polkit-kde-authentication-agent-1"
+    "/usr/libexec/polkit-gnome-authentication-agent-1"
+)
+
+for path in "${possible_paths[@]}"; do
+    if [ -x "$path" ]; then
+        exec "$path" &
+        break
+    fi
+done
+
+notify-send -u critical "No polkit agent found"
