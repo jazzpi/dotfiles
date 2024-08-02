f861314617adfd2fb36743aed178faf8f20329ee
jazzpi <jasper@mezzo.de>
2024-08-02 13:35:57 +0200

    i3: Use rofi for rand-restore dialog

--- i3/scripts/randr-restore.bash
+++ i3/scripts/randr-restore.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # What directory is this script in?
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if ! configs=$("$DIR/randr.bash" list); then
     i3-nagbar -m 'No configurations exist!'
     exit 1
 fi
 
-i3-input -F "exec '$DIR/randr.bash' restore '%s'" -P "Restore from [$configs]:"
+cfg=$(echo "$configs" | rofi -dmenu -sep ' ' -p "Restore from [$configs]:")
+if [ -z "$cfg" ]; then
+    exit 1
+fi
+"$DIR/randr.bash" restore "$cfg"
