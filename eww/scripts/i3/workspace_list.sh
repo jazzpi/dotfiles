9801fd9df2287efc852e4e50090f1934bf0a6673
jazzpi <jasper@mezzo.de>
2024-08-03 19:08:25 +0200

    eww: highlight visible, non-focused workspaces

--- eww/scripts/i3/workspace_list.sh
+++ eww/scripts/i3/workspace_list.sh
@@ -4,5 +4,5 @@
     echo initial # Trigger initial update
     i3-msg -t subscribe -m '[ "workspace" ]'
 ) | while IFS='' read line; do
-    i3-msg -t get_workspaces | jq -c 'group_by(.output) | map({(.[0].output): map({name, focused, urgent})}) | add'
+    i3-msg -t get_workspaces | jq -c 'group_by(.output) | map({(.[0].output): map({name, focused, urgent, visible})}) | add'
 done
