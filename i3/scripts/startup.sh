eb40da7103c8046e3c02b8f5f17e171f372ec786
jazzpi <jasper@mezzo.de>
2025-03-21 12:09:42 +0100

    i3: Add sleeps to startup script

--- i3/scripts/startup.sh
+++ i3/scripts/startup.sh
@@ -13,9 +13,11 @@ set -x
 
 # First, picom needs to run so other apps can use transparency
 poll 10 "pgrep -f picom" || true
+sleep 1
 # Then, EWW and Dunst need to run so that the tray and notifications are available
 ~/.config/eww/scripts/wm.sh launch &
 poll 10 '[ -z "$(eww active-windows)" -o -z "$(pgrep dunst)" ]' || true
+sleep 1
 
 # Then, we can start the rest of the applications
 thunderbird &
